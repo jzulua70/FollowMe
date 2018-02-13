9cb69c8987633f69ac71c2633e87453c23ff5f90
jzulua70 <jzulua70@eafit.edu.co>
2018-02-13 13:12:58 -0500

    corrigiendo errores

--- FollowMe/settings.py
+++ FollowMe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n&^@z#mxpz50+1s5(b@w58ui#jc$_*=7+lxa-yis8kr=$$(p=j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['followmee.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['followmee.herokuapp.com','localhost','10.131.137.230']
 
 
 # Application definition
