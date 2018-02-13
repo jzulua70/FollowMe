5fad8c4822f41d0342c6ee54d77288963a890ca3
jzulua70 <jzulua70@eafit.edu.co>
2018-02-12 21:05:59 -0500

    working

--- FollowMe/settings.py
+++ FollowMe/settings.py
@@ -157,5 +157,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
+
+
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
